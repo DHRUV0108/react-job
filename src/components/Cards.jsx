8ea22d19500ebfbb06106df4446cc36e1e244b58
Dhruv Singh <Dhrusinghjm@gmail.com>
2025-06-19 19:00:38 +0530

    card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Cards = ({children}) => {
+const Cards = ({title, sub, href,button, bg='bg-gray-100'}) => {
   return (
-    <div className='bg-gray-100 p-6 rounded-lg shadow-md'>{children}</div>
+    <>
+    <div className={`${bg}p-6 rounded-lg shadow-md`} >{children}</div>
+							<h2 classNrme="text-2xl font-bold">{title}</h2>
+							<p className="mt-2 mb-4">{sub}</p>
+							<a
+								href={href}
+								className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+							>{button}
+							</a>
+    </>
   )
 }
 
